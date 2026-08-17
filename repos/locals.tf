88557f7a35d52779fdce89a59bcf300fa458ddc4
Nikxn91 <nikolay.b.kostov@gmail.com>
2026-08-17 11:38:04 +0300

    task 8: create public tf-lab-app repository with team permissions and CODEOWNERS

--- repos/locals.tf
+++ repos/locals.tf
@@ -1,3 +1,10 @@
 locals {
   github_owner = "tf-lab-nikolay-kostov"
+
+  # Team slug => repository permission level
+  app_repository_team_permissions = {
+    "nodejs-team"   = "push"
+    "security-team" = "push"
+    "devops-team"   = "pull"
+  }
 }
---
+++ repos/repository.tf
@@ -0,0 +1,39 @@
+resource "github_repository" "tf_github_lab_app_repository" {
+  name        = "tf-lab-app"
+  description = "TF GitHub lab repository for nodejs app"
+
+  visibility           = "public"
+  vulnerability_alerts = true
+
+  has_issues   = true
+  has_projects = true
+  has_wiki     = true
+
+  allow_squash_merge     = true
+  allow_merge_commit     = true
+  allow_rebase_merge     = true
+  delete_branch_on_merge = true
+
+  auto_init = true
+}
+
+resource "github_team_repository" "tf_github_lab_app_repository" {
+  for_each = local.app_repository_team_permissions
+
+  team_id    = each.key
+  repository = github_repository.tf_github_lab_app_repository.name
+  permission = each.value
+}
+
+resource "github_repository_file" "tf_github_lab_app_repository_codeowners" {
+  repository = github_repository.tf_github_lab_app_repository.name
+  branch     = github_repository.tf_github_lab_app_repository.default_branch
+  file       = ".github/CODEOWNERS"
+
+  content = "* @${local.github_owner}/${data.terraform_remote_state.base.outputs.security_manager_team_slug}\n"
+
+  commit_message      = "chore: add CODEOWNERS managed by terraform"
+  commit_author       = "Terraform"
+  commit_email        = "terraform@tf-lab.local"
+  overwrite_on_create = true
+}
